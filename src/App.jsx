60bf6c3eeb2d74868051e61352f0c085e91db43b
camposCode <info@josecampos.io>
2022-08-25 19:06:04 +0200

    colornames library installed

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import { useState } from "react";
+import Input from "./components/Input";
+import Square from "./components/Square"; 
+
+
+function App() {
+  const[colorValue, setColorValue] = useState('');
+  const[hexValue, setHexValue] = useState('');
+  return (
+    <div className="App">
+      <Square 
+        colorValue={ colorValue }
+        hexValue={ hexValue }
+      />
+      <Input 
+        colorValue={ colorValue }
+        setColorValue={ setColorValue }
+        setHexValue={ setHexValue }
+      />
+    </div>
+  );
+}
+
+export default App;
